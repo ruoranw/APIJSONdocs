f8b5568556f6c54e0e17d308d41b35e47739ac31
Ruoran Wang <ruoran0502@gmail.com>
2019-10-26 00:27:48 -0700

    comment out something in conf.py

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,7 @@ release = '1.0.0'
 # ones.
 
 extensions = ['recommonmark',
-              'sphinx_markdown_tables'
+#              'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
